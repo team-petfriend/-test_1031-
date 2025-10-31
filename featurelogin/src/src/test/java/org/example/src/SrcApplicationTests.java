529029e49d0c38ab62e0ec99a32a4028a0eaed03
sinhyeok-Oh <sinhyeok1390@naver.com>
2025-10-31 12:28:02 +0900

    test

---
+++ featurelogin/src/src/test/java/org/example/src/SrcApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.src;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
